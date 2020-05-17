f75cabfbc0c47f05efff9a13c7e0a12c2930470e
Chandan Choudhary <chaudhary.chandan48@gmail.com>
2020-05-17 20:55:26 +0530

    login and signup static design implement

--- app/src/main/java/com/example/socialninjas/ui/auth/SignupActivity.kt
+++ app/src/main/java/com/example/socialninjas/ui/auth/SignupActivity.kt
@@ -1,12 +1,13 @@
-package com.example.socialninjas
+package com.example.socialninjas.ui.auth
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.socialninjas.R
 
-class MainActivity : AppCompatActivity() {
+class SignupActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_signup)
     }
 }
